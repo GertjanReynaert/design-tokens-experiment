9b14122c887c2036111ee9b9949fd23b2486d88a
Gertjan Reynaert <gertjan.reynaert@gmail.com>
2020-05-28 16:37:12 +0200

    Run style-dictionary init basic

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Thu, 28 May 2020 14:30:35 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
